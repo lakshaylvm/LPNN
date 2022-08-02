ee3119b347be0ff093e0e9f79a36aac32382cc4e
lakshaylvm <lakshaylvm@gmail.com>
2022-08-02 23:31:51 +0530

    first commit

---
+++ Resources/app.js
@@ -0,0 +1,43 @@
+const login_button = document.getElementById('pp');
+const signup_button = document.getElementById('up');
+
+const login_area = document.getElementById('log');
+const signup_area = document.getElementById('signups');
+
+const areaBlur = document.getElementById('blur');
+
+const logInLink = document.getElementById('signup-bttn');
+const signUpLink =document.getElementById('logs');
+
+const toggleLink = () =>{
+    login_area.classList.toggle('visible');
+    signup_area.classList.toggle('visible');
+}
+const removeDiv = function(){
+    login_area.classList.remove('visible');
+    signup_area.classList.remove('visible');
+    areaBlur.classList.remove('visible');
+};
+
+const toggle_blur = function(){
+    areaBlur.classList.toggle('visible');
+};
+
+
+const toggle_signup = function(){
+    signup_area.classList.toggle('visible');
+    toggle_blur();
+};
+
+const toggle_login = function(){
+    login_area.classList.toggle('visible');
+    toggle_blur();    
+};
+
+login_button.addEventListener('click',toggle_login);
+
+signup_button.addEventListener('click',toggle_signup);
+
+areaBlur.addEventListener('click',removeDiv);
+logInLink.addEventListener('click',toggleLink);
+signUpLink.addEventListener('click' , toggleLink);
